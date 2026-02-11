54c57b1c7713140bcf2ffa28595355a9a64ef1f7
maf1ck <kosmanmatvij@gmail.com>
2026-02-11 18:51:42 +0200

    initial repo strat doing header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<template lang="pug">
+    .header
+        .header_wrapper
+            .header__logo
+                img(src="")
+            .header__menu
+                ul.header__list
+                    li.header__item
+                        a.header__link(href="#") Home
+                    li.header__item
+                        a.header__link(href="#") Discount
+                    li.header__item
+                        a.header__link(href="#") Top Category
+                    li.header__item
+                        a.header__link(href="#") Best Sales
+            .header__search
+                input(type="text" placeholder="Search")
+            .header__cart
+                img(src="")
+
+                
+                    
+=
+</template>
+
+<style lang="scss">
+</style>
+
