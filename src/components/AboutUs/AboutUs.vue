038ebd99debae4a442324a0cfbf726bb27c5a499
maf1ck <kosmanmatvij@gmail.com>
2026-02-11 22:00:06 +0200

    start about us without styles

---
+++ src/components/AboutUs/AboutUs.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+
+</script>
+
+<template lang="pug">
+    .aboutus
+        .aboutus__description
+            .aboutus__description-wr
+                h1.aboutus__description_wr-title Your Ultimate Online
+                span.aboutus__description_wr-span Grocery
+                p.aboutus__description_wr-text Experience grocery shopping and swift home delivery with our wide range of fresh produce and essentials
+                button.aboutus__description_wr-btn Shop Now
+        .aboutus__image
+            img.aboutus__image-img(src="@/assets/about-us.jpeg" alt="About Us")
+        
+                
+</template>
+
+<style lang="scss" scoped>
+
+</style>
