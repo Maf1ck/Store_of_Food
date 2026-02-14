a2df992d0474d6bc9f06147850c97bec3c0c981b
Maf1ck <kosmanmatvij@gmail.com>
2026-02-15 01:13:53 +0200

    category v 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Header from "./components/Header/Header.vue";
 import Discounts from "./components/Discounts/Discounts.vue";
+import Category from "./components/Category/Category.vue";
 </script>
 
 <template>
   <Header/>
   <Discounts/>
+  <Category/>
 </template>
 
 <style scoped>
