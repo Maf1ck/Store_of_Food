4f8b7b81b1f0294b5f4580ccb7f62362f26258dd
Maf1ck <kosmanmatvij@gmail.com>
2026-02-15 20:42:47 +0200

    change variables

--- src/components/AboutUs/AboutUs.vue
+++ src/components/AboutUs/AboutUs.vue
@@ -4,12 +4,12 @@
 
 <template lang="pug">
     .aboutus
-        .aboutus__description
-            .aboutus__description-wr
-                h1.aboutus__description_wr-title Your Ultimate Online 
-                    span.aboutus__description_wr-span Grocery
-                p.aboutus__description_wr-text Experience grocery shopping and swift home delivery with our wide range of fresh produce and essentials
-                button.aboutus__description_wr-btn Shop Now
+        .aboutus__content
+            .aboutus__content-wr
+                h1.aboutus__content-wr-title Your Ultimate Online 
+                    span.aboutus__content-wr-span Grocery
+                p.aboutus__content-wr-text Experience grocery shopping and swift home delivery with our wide range of fresh produce and essentials
+                button.aboutus__content-wr-btn Shop Now
             .aboutus__image
                 img.aboutus__image-img(src="@/assets/about-us.svg" alt="About Us")
         
